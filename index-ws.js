92bf46e279dad9a236a229410d80defad820f633
Som E-Agwuegbo <sommykene@gmail.com>
2025-02-14 19:05:45 +0000

    add database logging

--- index-ws.js
+++ index-ws.js
@@ -13,28 +13,40 @@ server.listen(PORT, function () {
   console.log("Listening on " + PORT);
 });
 
+process.on("SIGINT", () => {
+  wss.clients.forEach(function each(client) {
+    client.close();
+  });
+  server.close(() => {
+    shutdownDB();
+  });
+});
+
 /** Websocket **/
-const WebSocketServer = require('ws').Server;
+const WebSocketServer = require("ws").Server;
 
 const wss = new WebSocketServer({ server: server });
 
-wss.on('connection', function connection(ws) {
+wss.on("connection", function connection(ws) {
   const numClients = wss.clients.size;
 
-  console.log('clients connected: ', numClients);
+  console.log("clients connected: ", numClients);
 
   wss.broadcast(`Current visitors: ${numClients}`);
 
   if (ws.readyState === ws.OPEN) {
-    ws.send('welcome!');
+    ws.send("welcome!");
   }
 
-  ws.on('close', function close() {
+  db.run(`INSERT INTO visitors (count, time)
+    VALUES (${numClients}, datetime('now'))`);
+
+  ws.on("close", function close() {
     wss.broadcast(`Current visitors: ${numClients}`);
-    console.log('A client has disconnected');
+    console.log("A client has disconnected");
   });
 
-  ws.on('error', function error() {
+  ws.on("error", function error() {
     //
   });
 });
@@ -45,9 +57,32 @@ wss.on('connection', function connection(ws) {
  * @void
  */
 wss.broadcast = function broadcast(data) {
-  console.log('Broadcasting: ', data);
+  console.log("Broadcasting: ", data);
   wss.clients.forEach(function each(client) {
     client.send(data);
   });
 };
 /** End Websocket **/
+/** begin database */
+const sqlite = require("sqlite3");
+const db = new sqlite.Database(":memory:");
+
+// .seralize ensures DB is set up before any queries
+db.serialize(() => {
+  db.run(`CREATE TABLE visitors (
+      count INTEGER,
+      time TEXT
+      )`);
+});
+
+function getCounts() {
+  db.each("SELECT * FROM visitors", (err, row) => {
+    console.log(row);
+  });
+}
+
+function shutdownDB() {
+  getCounts();
+  console.log("shutting down DB");
+  db.close();
+}
